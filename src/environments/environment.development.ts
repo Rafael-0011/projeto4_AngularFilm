9086dbcb729af30d4a03ccea0c3ac6830c54a703
rafael <rafaelsora@gmail.com>
2025-01-06 21:08:52 -0300

    mudando variavelDeAmbiente

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: false,
-  
-  TOKEM_TMDB: '',
 
-  URL_TRENDING: '',
+  TOKEM_TMDB:'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMGE0MDhlOGVjNDYzMWI4MDUxYTEzNDE0ZjU1YjNiYyIsIm5iZiI6MTczNTg0NDAwNi41MTUsInN1YiI6IjY3NzZlMGE2MmVmYmYzYjQxZDkyZGFiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M8UJn3ybqjz8Qk92R50745D8XaSPEsuWjUNjM8NSvRk',
 
-  URL_IMG: '',
+  URL_TRENDING:'https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1',
 
-  URL_TOP_RATED: '',
+  URL_IMG:'https://image.tmdb.org/t/p/w500',
 
-  URL_GETNAME_FILME_PARTE1: '',
+  URL_TOP_RATED:'https://api.themoviedb.org/3/movie/top_rated?language=pt-BR&page=1',
 
-  URL_GETNAME_FILME_PARTE2: '',
+  URL_GETNAME_FILME_PARTE1:'https://api.themoviedb.org/3/search/movie?query=',
 
-  URL_GET_DETAILS: '',
+  URL_GETNAME_FILME_PARTE2:'&include_adult=false&language=pt-BR',
+
+  URL_GET_DETAILS:'https://api.themoviedb.org/3/movie/',
 };
